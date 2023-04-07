6177286ba9e734741f132759307d71d7c70333ef
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-04-07 12:18:50 -0500

    Updating for errors

--- 0_run_all.R
+++ 0_run_all.R
@@ -1,4 +1,10 @@
 #This script is used to run all the analyses in this folder
 library(pbapply)
 targetScripts <- list.files(pattern = "Script_fitMultipleDatasets")
-pblapply(targetScripts, source)
+pblapply(targetScripts, function(x){
+  tryCatch({
+    source(x)
+  }, error=function(e){ 
+    write(x, "Error.txt", append = TRUE)
+    })
+})
