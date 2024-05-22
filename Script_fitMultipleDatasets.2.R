36e78600706856dbfcd6449c9fdd665a83122777
Cristian Román-Palacios <cromanpa94@gmail.com>
2024-05-22 07:24:31 -0700

    Remove those groups with less than 10 unique obs in either temp or d47

--- Script_fitMultipleDatasets.2.R
+++ Script_fitMultipleDatasets.2.R
@@ -3,12 +3,12 @@ library(pbapply)
 library(deming)
 library(pbmcapply)
 library(IsoplotR)
-library(R2jags)
 library(data.table)
 library(tictoc)
 library(rlist)
 library(dplyr)
 library(loo)
+library(bayclumpr)
 source("functions.R")
 
 synData$TempError <- ifelse(synData$TempError ==0, 1E-5, synData$TempError)
@@ -17,9 +17,14 @@ targetColumns <- colnames(synData)[c(12)]
 targetLevels<- names(which(table(synData[,targetColumns])>10))
 synData <- synData[synData[,targetColumns] %in% targetLevels ,]
 
+targetLevels2 <- tapply(synData$Temperature, synData[,targetColumns], function(x) length(unique(x)))
+targetLevels <- names(which(targetLevels2>10))
+synData <- synData[synData[,targetColumns] %in% targetLevels ,]
 
 SynthesisResults <- fitsinglePartitioned(
   calData = synData,
   targetColumns = targetColumns,
   prefix = paste0("Synthesis_",colnames(synData)[c(12)],"_", Sys.Date())
 )
+
+
